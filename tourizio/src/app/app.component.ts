4cd0e03a9e3546049231bd6dfec0423c6728c632
M-Nikhita <nikhitaalexander@gmail.com>
2025-09-23 21:41:59 +0530

    Milestone 3

--- tourizio/src/app/app.component.ts
+++ tourizio/src/app/app.component.ts
@@ -2,14 +2,25 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet} from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent,FooterComponent],
+  imports: [RouterOutlet, NavbarComponent,FooterComponent,CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class AppComponent {
   protected readonly title = signal('tourizio');
+  showFooter: boolean = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.showFooter = !this.router.url.includes('/login') && !this.router.url.includes('/signup');
+      }
+    });
+  }
 }
